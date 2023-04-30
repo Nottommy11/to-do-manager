b095ebb1a4df842f574162eef7c61eef17855344
Nottommy11 <thomas.marxsen11@gmail.com>
2023-04-30 00:25:05 -0500

    more stuff

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  home: "/to-do-manager/",
+  base: "/to-do-manager/",
   plugins: [react()],
 });
